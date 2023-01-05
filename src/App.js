8738df806cc1dcf4ee2911bc0cd1d6731e2ffc13
moin105 <moinlatif105@gmail.com>
2023-01-05 15:07:50 +0500

    gsa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import  SignaturePad from 'react-signature-canvas';
 function App() {
   return (
     <div className="App">
-      <SignaturePad penColor='green'
+      <SignaturePad penColor='black'
     canvasProps={{width: "1000", height: "1000", className: 'sigCanvas'}} />,
     </div>
   );
