a07cd9b13ceb617d1453df9904571226052e6378
moin105 <moinlatif105@gmail.com>
2023-01-05 14:54:01 +0500

    orchestral

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import  SignaturePad from 'react-signature-canvas';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignaturePad penColor='green'
+    canvasProps={{width: 500, height: 200, className: 'sigCanvas'}} />,
     </div>
   );
 }
