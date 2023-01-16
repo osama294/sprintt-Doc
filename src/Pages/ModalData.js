4709b17a32196c26e2bc3605ddfa896f47e3a213
moin105 <moinlatif105@gmail.com>
2023-01-16 18:10:29 +0500

    dewf

--- src/Pages/ModalData.js
+++ src/Pages/ModalData.js
@@ -59,6 +59,7 @@ function ModalData({ setTab, setActiveLast, count, date, itemDetails, itemCreds,
         const requestOptions = {
             method: "POST",
             headers: {
+                mode: "no-cors",
                 "Content-Type": "application/json",
                 'Access-Control-Allow-Credentials': true,
                 'Access-Control-Allow-Origin': '*',
